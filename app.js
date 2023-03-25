a9cca96c2192f91afd3138eb987577051e575af8
MossaJehad <Moosa.Jehad65@gmail.com>
2023-03-26 00:56:59 +0300

    CRUDS grocery store

--- app.js
+++ app.js
@@ -23,8 +23,14 @@ app.use(methodOverride('_method'))
 const categories = ['fruit', 'vegetable', 'dairy'];
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index.ejs', { products })
+    const {category} = req.query;
+    if(category){
+        const products = await Product.find({category})
+        res.render('products/index.ejs', { products, category })
+    }else {
+        const products = await Product.find({})
+        res.render('products/index.ejs', { products, category: 'All' })
+    }
 })
 
 app.get('/products/new', (req, res) => {
@@ -53,6 +59,12 @@ app.put('/products/:id', async (req, res) => {
     const {id} = req.params
     const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
     res.redirect('/products')
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const {id} = req.params
+    const deletedProduct = await Product.findByIdAndDelete(id)
+    res.redirect('/products');
 })
 
 app.listen(3000, () => {
