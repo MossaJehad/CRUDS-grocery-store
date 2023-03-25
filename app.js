3e63c4cdbaa6799c3c385f3e65ebbaf8908507f2
MossaJehad <Moosa.Jehad65@gmail.com>
2023-03-26 00:25:08 +0300

    CRUDS grocery store

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 const Product = require('./models/product')
 
@@ -15,7 +16,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+const categories = ['fruit', 'vegetable', 'dairy'];
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
@@ -23,7 +28,7 @@ app.get('/products', async (req, res) => {
 })
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new.ejs')
+    res.render('products/new.ejs', {categories})
 })
 
 app.post('/products', async (req, res) => {
@@ -35,8 +40,19 @@ app.post('/products', async (req, res) => {
 app.get('/products/:id', async(req, res) => {
     const { id } = req.params;
     const foundProduct = await Product.findById(id)
-    console.log(foundProduct)
     res.render('products/show', {foundProduct})
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', {product, categories} )
+})
+
+app.put('/products/:id', async (req, res) => {
+    const {id} = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
+    res.redirect('/products')
 })
 
 app.listen(3000, () => {
